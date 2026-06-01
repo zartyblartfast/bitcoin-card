ca1a100895e9b3987883700393e342dff9bee3d4
zartyblartfast <[email protected]>
2026-06-01 14:12:10 +0100

    chore(data): export full public API and add @types/node for build

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -2,3 +2,7 @@ export * from "./types.js";
 export { getPrice } from "./getPrice.js";
 export { getBlockHeight } from "./getBlockHeight.js";
 export { getMempoolFees } from "./getMempoolFees.js";
+export { getFeeHistory } from "./getFeeHistory.js";
+export { getNetworkSummary } from "./getNetworkSummary.js";
+export { getUnminedSupply } from "./getUnminedSupply.js";
+export { computeUnminedSupply, computeCurrentSupply } from "./derive/unmined.js";
